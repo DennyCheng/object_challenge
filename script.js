ff6be244fd0191a502a297fc0133d5f55ad1a2f4
dennycheng <chen2368@umn.edu>
2016-07-28 09:27:24 -0500

    completed the challenge

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function Pizza(state,revenue,locationsCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationsCount;
+  this.averageRevenue = function(){
+    return(this.revenue/this.locationCount);
+  }
+};
 // object instances
-
+var Ill = new Pizza("Il",5000,12);
+var Mn = new Pizza("MN",300,3);
+var Nv = new Pizza("NV",25000,1);
 // push object instances to locations array
+//locations.push(new Pizza("Il",5000,12));
+locations.push(Mn);
+locations.push(Ill);
+locations.push(Nv);
+console.log(locations);
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
